c28f616153e66aea6cea1bf3ce93c5d98a47438b
Ademar Castro <ademar.castro.fh@gmail.com>
2025-01-20 23:01:56 -0400

    [INIT]: Initial project setup with basic structure and dependencies.

---
+++ src/main/java/ifam/nutriveg/NutrivegApplication.java
@@ -0,0 +1,13 @@
+package ifam.nutriveg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrivegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrivegApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ifam/nutriveg/NutrivegApplicationTests.java
@@ -0,0 +1,13 @@
+package ifam.nutriveg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrivegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
